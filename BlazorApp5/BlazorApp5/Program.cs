140db4840f5a846781d93c2d9c12b3d53458c5f3
mostafa jafari <jafari.mostafa@bcmlogic.com>
2024-03-09 17:38:11 +0100

    Add project files.

---
+++ BlazorApp5/BlazorApp5/Program.cs
@@ -0,0 +1,46 @@
+using BlazorApp5.Client.Pages;
+using BlazorApp5.Components;
+
+namespace BlazorApp5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveWebAssemblyComponents();
+
+            builder.Services.AddScoped(sp =>
+                    new HttpClient
+                    {
+                    });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+            app.Run();
+        }
+    }
+}
